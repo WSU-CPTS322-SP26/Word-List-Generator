408608de31aef18c8b17b1fb559545c6cb13cad9
shanehg0451 <shanehg0451@gmail.com>
2026-03-04 20:06:44 -0800

    Created engine worker and runPipeline functions, started main, added sample wordlist for testing, minor edits to collector and mutations

---
+++ src/engine.go
@@ -0,0 +1,37 @@
+package main
+
+// Recursive function that takes a single word through all its mutations
+func runPipeline(word string, stage int, pipeline [][]Mutation, scratchpads [][]string, outputChan chan<- string) {
+	//Base case
+	if stage == len(pipeline) {
+		outputChan <- word
+		return
+	}
+
+	//This outer loop iterates through every "or" alternative for this specific stage
+	for _, mutation := range pipeline[stage] {
+
+		//Runs mutation and collects every variant of the word output by the current mutation
+		//This also resets that stage's scratchpad to length 0 (erasing its contents)
+		variants := mutation(word, scratchpads[stage][:0])
+
+		//The inner loop iterates through all the different variants produced and recurs them into their own next stage of the pipeline
+		for _, variant := range variants {
+			runPipeline(variant, stage+1, pipeline, scratchpads, outputChan)
+		}
+	}
+}
+func worker(inputChan <-chan string, pipeline [][]Mutation, outputChan chan<- string) {
+	//Making buffers. These only serve this specific worker, one for each stage in the pipeline
+	//Each stage needs its own buffer/scratchpad
+	scratchpads := make([][]string, len(pipeline))
+	for i := range scratchpads {
+		//Capacity is preset
+		scratchpads[i] = make([]string, 0, 256)
+	}
+
+	//Now, begin running each word in the input channel through the pipeline of Mutatins
+	for word := range inputChan {
+		runPipeline(word, 0, pipeline, scratchpads, outputChan)
+	}
+}
